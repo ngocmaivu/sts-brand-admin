32378f5a935c711f34a091a91bb3a7f6189fef67
mai vu <maivu629@gmail.com>
2021-06-02 13:47:24 +0700

    [maivtn] update webpack config
    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx',],  
+        extensions: ['.js', '.jsx', '.css'],
     },
     module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-            }
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.jsx$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }
         ]
     },
     plugins: [new HtmlWebpackPlugin({
         template: './src/index.html'
-    })],
+    }), new MiniCssExtractPlugin()],
     devServer: {
         historyApiFallback: true
     },
