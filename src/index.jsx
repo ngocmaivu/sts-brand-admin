cfe0d18c92487104a44dc49e313d9c45da7c8d02
mai vu <maivu629@gmail.com>
2021-06-02 13:48:23 +0700

    [maivtn] update index
    add bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import { store } from './_helpers';
 import { App } from './App';
 
